51fd80151bc5099cc193495fd8c360c2a0f08472
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-03 22:10:24 +0000

    Move setupInput function into input.js

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-// Keyboard input
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    // Escape out of game with Ctrl + C (close stdin connection)
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };  
-
-  // Event listener for keyboard input
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
 
 setupInput();
