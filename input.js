8a8d6a6ff8ce70bc20306cf9e2074c25f5ed249b
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-03 23:09:48 +0000

    Connect keyboard input to server

--- input.js
+++ input.js
@@ -1,10 +1,29 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   const handleUserInput = function (key) {
+    // Move up with w
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
+
+    
     // Escape out of game with Ctrl + C (close stdin connection)
     if (key === '\u0003') {
       process.exit();
