3307b98c8b7ccc54d8cf25c6594c9b10a6d82366
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-03 19:21:42 +0000

    Add function to keyboard input & can exit with Ctrl + C

--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// Keyboard input
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    // Escape out of game with Ctrl + C (close stdin connection)
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };  
+
+  // Event listener for keyboard input
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
